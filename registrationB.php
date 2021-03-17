04c7c2d95ed5d440bc7a2e4b87079e44fa770d4f
Xavier <xavier.dandigna@edu.ece.fr>
2021-03-17 15:42:39 +0100

    liaison bdd non finis

---
+++ registrationB.php
@@ -0,0 +1,40 @@
+<?php
+$server_name="localhost:3307";
+$username="root";
+$password="";
+$database_name="cykel";
+
+$conn=mysqli_connect($server_name,$username,$password,$database_name);
+
+if(!$conn)
+{
+    die("Connection Failed:".mysqli_connect_error());
+    
+}
+
+if(isset($_POST['save']))
+{
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+    $firstName=$_POST['firstName'];
+    $adress=$_POST['adress'];
+    $city=$_POST['city'];
+    $zipCode=$_POST['zipCode'];
+    $country=$_POST['country'];
+    $phone=$_POST['phone'];
+    
+    $sql_query = "INSERT INTO buyer (firstName,lastName,photo,legalContract, paymentMethod, adress, city, zipCode, country, phone, username, password) VALUES ('$first_name','$last_name','$gender','$email','$phone')";
+    
+    if(mysqli_query($conn, $sql_query))
+    {
+        echo ("OK");
+    }
+    else
+    {
+        echo ("ERROR");
+    }
+    mysqli_close($conn);
+    
+}
+?>
+
