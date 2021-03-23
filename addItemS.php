14c4d4424d5bfc1631a602496555db3c6ab29f24
Xavier <xavier.dandigna@edu.ece.fr>
2021-03-23 15:06:23 +0100

    up

--- addItemS.php
+++ addItemS.php
@@ -56,6 +56,7 @@ $status = $statusMsg = '';
     
 $content = "<html><head></head><body>Welcome NOW : $name , $price , $category , $currentUsername</body></html>";
 $file = "newpage.html";
+    
 file_put_contents($file, $content);
     
  
