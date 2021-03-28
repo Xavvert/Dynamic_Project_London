4f75598d2612e129a8aa7caf47c4f1d1b0df6745
ansailme <anthelme.charvet@edu.ece.fr>
2021-03-28 22:54:25 +0200

    checkout opé

--- checkout.php
+++ checkout.php
@@ -2,8 +2,8 @@
 session_start();
 
 ?>
-   
- 
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -52,29 +52,66 @@ session_start();
             font-size: 36px;
             margin-left: 50px;
         }
+
         .co {
             width: 800px;
-            height: 1000px;
+            height: auto;
             background-color: #EEE8CD;
             border-radius: 1em;
             color: black;
             text-align: center;
             font-size: 15x;
             font-family: "Verdana";
+            display: flex;
+            flex-direction: column;
+            margin-left: 320px;
+        }
 
+        .row {
+            display: flex;
+            justify-content: center;
+        }
+
+        .column {
+            display: flex;
+            flex-direction: column;
+            padding: 1em;
         }
 
         h5 {
             font-size: 30px;
         }
-        #logo:hover {
 
+        h1 {
+            color: darkblue;
+            text-align: center;
+            padding: 15px;
+        }
+
+        .img {
+            width: 100px;
+            position: relative;
+            animation: mymove 5s infinite;
+            margin-left: 330px;
+        }
+
+        @keyframes mymove {
+            from {
+                left: 0px;
+            }
+
+            to {
+                left:700px;
+            }
+        }
+
+        #logo:hover {
             transform: rotate(360deg);
             transition: all 1s;
         }
     </style>
-    
-      <style type="text/css" caption="form design">
+
+    <style type="text/css" caption="form design">
         * {
             box-sizing: border-box;
         }
@@ -89,18 +126,20 @@ session_start();
             border: 1px solid #ccc;
             border-radius: 4px;
             resize: vertical;
-
+            display: flex;
         }
 
         label {
             padding: 12px 12px 12px 0;
-            display: inline-block;
+            display: flex;
+
         }
 
-        input[type=submit],[type=reset] {
+        input[type=submit],
+        [type=reset] {
             background-color: whitesmoke;
             padding: 12px 20px;
-            border: solid 0,1em;
+            border: solid 0, 1em;
             border-radius: 4px;
             cursor: pointer;
         }
@@ -108,11 +147,10 @@ session_start();
         input[type=submit]:hover {
             background-color: #45a049;
         }
-        
-          input[type=reset]:hover {
+
+        input[type=reset]:hover {
             background-color: #bfb700;
         }
-
     </style>
 
 
@@ -163,98 +201,106 @@ session_start();
     <center>
         <h1>Please enter all the informations for the delivery process </h1>
     </center>
-   <center>
-        <div class="co">
+    
+    <img src="pictures/delivery.png" class="img">
+    
+    <div class="co">
+        <form action="tempCheckout.php" method="post">
+            <br>
+            <div class="row">
+                <div class="column">
 
-            <form action="tempCheckout.php" method="post">
-                <br> 
-                <label>Name your command </label>
-                <br><input type="text" name="packagename">
-                <br>
-                <label>First name</label>
-                <br><input type="text" name="firstname" value='<?php echo $_SESSION['firstname'] ?>'>
-                <br>
-                <label>Last name</label><br><input type="text" name="lastname" value='<?php echo $_SESSION['lastname'] ?>'>
-                <br>
-                <label>Adress</label><br><input type="text" name="adress" value='<?php echo $_SESSION['adress'] ?>'>
-                <br>
-               <label>City</label><br><input type="text" name="city" value='<?php echo $_SESSION['city'] ?>'>
-                <br>
-                <label>Zip Code</label><br><input type="text" name="zipCode" value='<?php echo $_SESSION['zipCode'] ?>'>
-                <br>
-                <label>Country</label><br><input type="text" name="country" value='<?php echo $_SESSION['country'] ?>'>
-                <br>
-                <label>Phone Number</label><br><input type="text" name="phone" value='<?php echo $_SESSION['phone'] ?>'>
-                <br><br>
-                <input type="submit" name="save" value="I confirm my informations and process to payment *">
-                <br>
-                <br>
-                <input type="reset" name="save" value="Reset">
-                <br>
-                <p style="font-size:12px; padding:35px;">* By clicking on <a href="privacypolicy.html" style="color:green;">"I confirm my informations and process to payment"</a>, you are agree that all of your informations are correct and that you will be redirect on the payment interface. You won't be able to go back until the payment is finalized.</p>
-            </form>
+                    <label>Name your command </label>
+                    <input type="text" name="packagename">
+                    <br>
+                    <label>First name</label>
+                    <input type="text" name="firstname" value='<?php echo $_SESSION['firstname'] ?>'>
+                    <br>
+                    <label>Last name</label><input type="text" name="lastname" value='<?php echo $_SESSION['lastname'] ?>'>
+                    <br>
+                    <label>Adress</label><input type="text" name="adress" value='<?php echo $_SESSION['adress'] ?>'>
+                    <br>
+                </div>
 
+                <div class="column">
+                    <label>City</label><input type="text" name="city" value='<?php echo $_SESSION['city'] ?>'>
+                    <br>
+                    <label>Zip Code</label><input type="text" name="zipCode" value='<?php echo $_SESSION['zipCode'] ?>'>
+                    <br>
+                    <label>Country</label><input type="text" name="country" value='<?php echo $_SESSION['country'] ?>'>
+                    <br>
+                    <label>Phone Number</label><input type="text" name="phone" value='<?php echo $_SESSION['phone'] ?>'>
+                    <br><br>
+                </div>
+            </div>
+            <input type="submit" name="save" value="I confirm my informations and process to payment *">
+            <br>
+            <br>
+            <input type="reset" name="save" value="Reset">
+            <br>
+            <p style="font-size:12px; padding:35px;">* By clicking on <a href="privacypolicy.html" style="color:green;">"I confirm my informations and process to payment"</a>, you are agree that all of your informations are correct and that you will be redirect on the payment interface. You won't be able to go back until the payment is finalized.</p>
+        </form>
+
+    </div>
+    <br>
+    <br>
+
+    <!--footer-->
+
+    <footer class="footer">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3">
+                    <br>
+                    <h4 class="title">Cykel</h4>
+                    <p>Cykel market is an auction website similar to EBay created for the Dynamic Web Programming Project.</p>
+                    <p style="color: #000058;">INSEEC U London - Y3</p>
+
+                    <ul class="social-icon">
+                        <a href="#" class="social"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                        <a href="#" class="social"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                        <a href="#" class="social"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                        <a href="#" class="social"><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
+                        <a href="#" class="social"><i class="fa fa-google" aria-hidden="true"></i></a>
+                    </ul>
+                </div>
+                <div class="col-sm-3">
+                    <br>
+                    <h4 class="title">Policy</h4>
+                    <span class="acount-icon">
+                        <a href="privacypolicy.html"><i class="fa fa-user" aria-hidden="true"></i> Privacy Policy</a>
+                        <a href="termsofservice.html"><i class="fa fa-file" aria-hidden="true"></i> Terms Of Service</a>
+                        <a href="mailto: contact@cykel.com"><i class="fa fa-envelope-o" aria-hidden="true"></i> Contact Us</a>
+                    </span>
+                </div>
+                <div class="col-sm-3">
+                    <br>
+                    <h4 class="title">Category</h4>
+                    <div class="category">
+                        <a href="subcatBike.html">All Bikes 🚲</a>
+                        <a href="subcatboard.html">All Boardgames 🎲</a>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <br>
+                    <h4 class="title">Payment Methods</h4>
+                    <p>This website accept this payment methods.</p>
+                    <ul class="payment">
+                        <li><a href="#"><i class="fa fa-cc-amex" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-cc-mastercard" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-paypal" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-cc-visa" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-apple" aria-hidden="true"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <hr>
+
+            <div class="row text-center" style="color: #000058;"> © 2021 Designed with by Xavier Dandigna & Anthelme Charvet.</div>
         </div>
-       <br>
-       <br>
-    </center>
 
-<!--footer-->
 
-<footer class="footer">
-
-    <div class="container">
-        <div class="row">
-        <div class="col-sm-3">
-            <br>
-            <h4 class="title">Cykel</h4>
-            <p>Cykel market is an auction website similar to EBay created for the Dynamic Web Programming Project.</p>  
-            <p style="color: #000058;">INSEEC U London - Y3</p>
-
-            <ul class="social-icon">
-            <a href="#" class="social"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-            <a href="#" class="social"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-            <a href="#" class="social"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-            <a href="#" class="social"><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
-            <a href="#" class="social"><i class="fa fa-google" aria-hidden="true"></i></a>
-            </ul>
-            </div>
-        <div class="col-sm-3">
-            <br>
-            <h4 class="title">Policy</h4>
-            <span class="acount-icon">          
-            <a href="privacypolicy.html"><i class="fa fa-user" aria-hidden="true"></i> Privacy Policy</a>
-            <a href="termsofservice.html"><i class="fa fa-file" aria-hidden="true"></i> Terms Of Service</a>
-            <a href = "mailto: contact@cykel.com"><i class="fa fa-envelope-o" aria-hidden="true"></i> Contact Us</a>       
-          </span>
-            </div>
-        <div class="col-sm-3">
-            <br>
-            <h4 class="title">Category</h4>
-            <div class="category">
-            <a href="subcatBike.html">All Bikes 🚲</a> 
-            <a href="subcatboard.html">All Boardgames 🎲</a>         
-            </div>
-            </div>
-        <div class="col-sm-3">
-            <br>
-            <h4 class="title">Payment Methods</h4>
-            <p>This website accept this payment methods.</p>
-            <ul class="payment">
-            <li><a href="#"><i class="fa fa-cc-amex" aria-hidden="true"></i></a></li>
-            <li><a href="#"><i class="fa fa-cc-mastercard" aria-hidden="true"></i></a></li>            
-            <li><a href="#"><i class="fa fa-paypal" aria-hidden="true"></i></a></li>
-            <li><a href="#"><i class="fa fa-cc-visa" aria-hidden="true"></i></a></li>
-            <li><a href="#"><i class="fa fa-apple" aria-hidden="true"></i></a></li>
-            </ul>
-            </div>
-        </div>
-        <hr>
-        
-        <div class="row text-center" style="color: #000058;"> © 2021 Designed with by Xavier Dandigna & Anthelme Charvet.</div>
-        </div>
-        
-        
     </footer>
 </body>
 
