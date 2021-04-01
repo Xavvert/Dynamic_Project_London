71f98488b0e3ddca90ef081cfac859a4136e4448
Xavier <xavier.dandigna@edu.ece.fr>
2021-04-01 12:17:39 +0200

    up

--- bid.php
+++ bid.php
@@ -55,7 +55,17 @@ $date2=date('Y-m-d');
 
 if ($date2 > $date1)
 {
-        $sql_query = mysqli_query($conn, "UPDATE bid SET status= 'Finalized' WHERE name='$retrievedName'");
+
+    $sql_query = mysqli_query($conn, "UPDATE bid SET status= 'Finalized' WHERE name='$retrievedName'");
+
+$sql_query = mysqli_query($conn, "Select * from bid WHERE name= '$retrievedName'");
+$row = mysqli_fetch_array($sql_query);
+$goodPrice=$row['price'];
+$goodBuyer=$row['id_buyer'];
+$sql_query = mysqli_query($conn, "UPDATE item SET type= 'bid' WHERE name='$retrievedName'");
+$sql_query = mysqli_query($conn, "UPDATE item SET price= '$goodPrice' WHERE name='$retrievedName'");
+$sql_query = mysqli_query($conn, "UPDATE item SET id_buyer= '$goodBuyer' WHERE name='$retrievedName'");
+
         header("location:Warningover.html");
         mysqli_close($conn);
 }
